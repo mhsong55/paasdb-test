3aedd72f81d1360ea801eb9adcc3d73dfc9182d1
MyungHoon Song <smh90510@gmail.com>
2021-12-13 23:13:46 +0900

    first

---
+++ src/main/java/com/mhsong/paasdbconntest/PaasdbConnTestApplication.java
@@ -0,0 +1,13 @@
+package com.mhsong.paasdbconntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasdbConnTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaasdbConnTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhsong/paasdbconntest/PaasdbConnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhsong.paasdbconntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaasdbConnTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
