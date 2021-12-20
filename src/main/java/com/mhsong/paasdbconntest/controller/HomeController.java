c56e32c4ee4f166f0c74a1363400dc3223b9d833
MyungHoon Song <smh90510@gmail.com>
2021-12-20 16:05:47 +0900

    merge conflict 해결

--- src/main/java/com/mhsong/paasdbconntest/controller/HomeController.java
+++ src/main/java/com/mhsong/paasdbconntest/controller/HomeController.java
@@ -1,9 +1,4 @@
 package com.mhsong.paasdbconntest.controller;
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 77be04b410cae12be36d53fee30cc2337f06c4f4
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,31 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-<<<<<<< HEAD
     final static Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home() {
-        logger.info("HOME CONTROLLER, PATH : /test");
-        return "PATH : / <br>SPRING WAR TEST";
+        logger.info("HOME CONTROLLER, PATH : /");
+        return "PATH : /";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
         logger.info("HOME CONTROLLER, PATH : /test");
         return "PATH : /test";
-=======
-    static Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String home() {
-        logger.info("HOME CONTROLLER, PATH: /");
-        return "PATH: /";
-    }
-    @RequestMapping(method = RequestMethod.GET, value = "/test")
-    public String test() {
-        logger.info("HOME CONTROLLER, PATH: /test");
-        return "PATH: /test";
->>>>>>> 77be04b410cae12be36d53fee30cc2337f06c4f4
     }
 }
