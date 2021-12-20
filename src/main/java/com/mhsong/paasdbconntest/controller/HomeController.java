b864e55a6b3674894721cb010f87ceedc5c7ae47
MyunHoonSong <smh90510@gmail.com>
2021-12-20 15:25:43 +0900

    사무실 PC 작업내용

---
+++ src/main/java/com/mhsong/paasdbconntest/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.mhsong.paasdbconntest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    final static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String home() {
+        logger.info("HOME CONTROLLER, PATH : /test");
+        return "PATH : / <br>SPRING WAR TEST";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public String test() {
+        logger.info("HOME CONTROLLER, PATH : /test");
+        return "PATH : /test";
+    }
+}
